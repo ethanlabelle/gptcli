2e4e46c90e1f8b6be669100b9352eeb298943d3a
Luke <anger.luke2004@gmail.com>
2023-07-03 20:32:57 -0400

    I added the ability to add websites to the prompt but I think I need to extraact the text to limit the size and maybe even limit the size more. Also there is now a setup for future commands

--- chatgpt.py
+++ chatgpt.py
@@ -1,27 +1,72 @@
 #!/usr/bin/python3
 import openai
+import requests
 log = True
+special_start="<?"
+special_end="?>"
+
+def send_message(request, log = False):
+    """
+    This function takes in a request and sends it and returns the response
+
+    Args:
+        request (string): The message to be sent
+        log (boolean): If the message should be logged
+
+    Returns:
+        string: the response
+    """
+    # TODO: use the saved logs for responses
+    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": request}])
+    content = chat_completion.choices[0].message.content
+
+    if log:
+        with open("log.txt", "a") as f:
+            f.write(str({request: dict(chat_completion)}) + "\n")
+
+    return content
+
+def convert_commands_to_text(line):
+    """
+    This function converts special commands and returns their text
+
+    Args:
+        line (string): line to be converted
+
+    Returns:
+        string: the response
+    """
+    try:
+        start=line.index(special_start)
+        end=line.index(special_end)
+    except (ValueError):
+        return line
+    
+    command=line[start+len(special_start):end]
+    # print(f'the command before the url is {command}')
+    if command[:4] == "url=":
+        # print(f'the command in the if is {command}')
+        command = requests.get(command[4:]).text
+        # print(f'the html code type is {type(command)}')
+
+    return convert_commands_to_text(line[:start] + command + line[end+len(special_end):])
 
 if __name__ == '__main__':
     print("STARTING REPL...")
     print("================")
-    line = input()
+    line = convert_commands_to_text(input())
     if line == "list models":
         models = openai.Model.list()
         print(models)
         print(models.data)
     else:
         while line != "quit":
-            with open("log.txt", "a") as f:
-                chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": line}])
-                content = chat_completion.choices[0].message.content
-                print(content)
+            
+            content = send_message(line, log)
+            print(content)
 
-                if log:
-                    f.write(str({line: dict(chat_completion)}) + "\n")
-
-                next_line = input()
-                if next_line != "quit":
-                    line = line + ';' + content + ';' + next_line
-                else:
-                    line = next_line
+            next_line = convert_commands_to_text(input())
+            if next_line != "quit":
+                line = line + ';' + content + ';' + next_line
+            else:
+                line = next_line
